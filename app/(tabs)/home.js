551e31fb9c108e4daf23d02992de9d3e237a3152
harfouche <gabrielh@live.fr>
2024-02-15 15:08:20 +0100

    Add Home Page

--- app/(tabs)/home.js
+++ app/(tabs)/home.js
@@ -2,12 +2,12 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import Onboarding from '../../components/Onboarding';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Informations</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Onboarding />
     </View>
   );
 }
---
+++ slides.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: '1',
+        title: '1 Quick & easy Payments',
+        description: 'Grow your business by accepting card payments with the new card render',
+        image: require('./assets/images/creditcard.png'),
+    },
+    {
+        id: '2',
+        title: '2 Quick & easy Payments',
+        description: 'Grow your business by accepting card payments with the new card render',
+        image: require('./assets/images/creditcard.png'),
+    },
+    {
+        id: '3',
+        title: '3 Quick & easy Payments',
+        description: 'Grow your business by accepting card payments with the new card render',
+        image: require('./assets/images/creditcard.png'),
+    },
+    {
+        id: '4',
+        title: '4 Quick & easy Payments',
+        description: 'Grow your business by accepting card payments with the new card render',
+        image: require('./assets/images/creditcard.png'),
+    },
+];
